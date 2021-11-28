6465c764586d1935c10a41e6d852e974d5c471c0
David Diaz <80727430+daviddiazh@users.noreply.github.com>
2021-11-27 20:51:27 -0500

    Se borra el directorio del back para bajar dependencias de mysql

--- back/src/main/java/co/com/sofka/crud/repositories/TaskListRepository.java
+++
@@ -1,8 +0,0 @@
-package co.com.sofka.crud.repositories;
-
-import co.com.sofka.crud.models.TaskList;
-import org.springframework.data.repository.CrudRepository;
-
-public interface TaskListRepository extends CrudRepository<TaskList, Long> {
-
-}
